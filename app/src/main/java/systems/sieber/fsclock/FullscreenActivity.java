e4c0e837f290b93d55c243f22ed82111f596f5fa
Georg Sieber <it@georg-sieber.de>
2022-10-17 10:53:41 +0200

    fix event label not hidden if no events found on startup on older devices

--- app/src/main/java/systems/sieber/fsclock/FullscreenActivity.java
+++ app/src/main/java/systems/sieber/fsclock/FullscreenActivity.java
@@ -106,6 +106,9 @@ public class FullscreenActivity extends AppCompatActivity {
                 }
             });
         }
+
+        // initial event state update
+        mContentView.updateEventView();
     }
 
     @Override
