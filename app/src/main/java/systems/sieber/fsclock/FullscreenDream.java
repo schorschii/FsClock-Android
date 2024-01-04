b2fed800da1885a4b40d2c0f06cc0db466133bf0
Georg Sieber <it@georg-sieber.de>
2024-01-04 22:09:16 +0100

    ugly workaround for rotation in screensaver mode (landscape version of view_fsclock is not applied automatically in screensaver mode, seems to be an Android bug)

--- app/src/main/java/systems/sieber/fsclock/FullscreenDream.java
+++ app/src/main/java/systems/sieber/fsclock/FullscreenDream.java
@@ -4,8 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.BatteryManager;
 import android.os.Build;
+import android.os.Handler;
 import android.service.dreams.DreamService;
 import android.view.View;
 
@@ -18,13 +20,18 @@ public class FullscreenDream extends DreamService {
         super.onAttachedToWindow();
         setInteractive(false);
         setFullscreen(true);
+        //setScreenBright(false); // doesn't seem to make a difference - or I don't understand it
+        initContentView();
+        registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    private void initContentView() {
         setContentView(R.layout.dream_fullscreen);
 
         // find views
         mContentView = findViewById(R.id.fullscreen_fsclock_view);
-        //setScreenBright(false); // doesn't seem to make a difference - or I don't understand it
 
-        // hide the system navigation bar with the same flags as done in the Android clock app
+        // hide the system navigation bar with the same flags as done in the pre-installed Android clock app
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -32,9 +39,6 @@ public class FullscreenDream extends DreamService {
                     | View.SYSTEM_UI_FLAG_IMMERSIVE
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
-
-        // init battery info
-        registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
     @Override
@@ -61,6 +65,23 @@ public class FullscreenDream extends DreamService {
         try {
             unregisterReceiver(this.mBatInfoReceiver);
         } catch(IllegalArgumentException ignored) {}
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // re-init the content view
+        // this is a really annoying Android bug: when rotating in screensaver mode,
+        // the view_fsclock layout is broken / not automatically updated to landscape
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initContentView();
+                mContentView.resume();
+            }
+        }, 100);
     }
 
     private final BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
