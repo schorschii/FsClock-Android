135af121830bece907516a13dafa13c79ff7fb2c
Alex Mi <alexmi3.14@gmail.com>
2025-03-14 13:51:38 -0400

    Make unlock settings free by default

--- app/src/main/java/systems/sieber/fsclock/BaseFeatureCheck.java
+++ app/src/main/java/systems/sieber/fsclock/BaseFeatureCheck.java
@@ -32,12 +32,12 @@ class BaseFeatureCheck {
     void init() {
         // get settings (faster than google play - after purchase done, billing client needs minutes to realize the purchase)
         mSettings = mContext.getSharedPreferences(SettingsActivity.SHARED_PREF_DOMAIN, 0);
-        unlockedSettings = mSettings.getBoolean("purchased-settings", false);
+        unlockedSettings = mSettings.getBoolean("purchased-settings", true);
     }
 
     boolean isReady = false;
 
-    boolean unlockedSettings = false;
+    boolean unlockedSettings = true;
 
     @SuppressWarnings("SwitchStatementWithTooFewBranches")
     void unlockPurchase(String sku) {
